1dfee617ee673d87d5af2cb91909cb853f619990
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-03-27 15:41:29 +0100

    :sparkles: feat(footer): adds a footer

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './style.scss';
 import printStartPage from './script/printStartPage.ts';
 import printNav from './script/printNav.ts';
+import printFooter from './script/printFooter.ts';
 
 printNav();
 printStartPage();
+printFooter();
---
+++ src/script/printFooter.ts
@@ -0,0 +1,20 @@
+export default function printFooter() {
+  const footerContainer: HTMLHeadElement = document.createElement('footer');
+  footerContainer.classList.add('footer');
+
+  const iconImg: HTMLImageElement = document.createElement('img');
+  iconImg.src = './img/travels_icon_color.webp';
+  iconImg.classList.add('icon_footer_img');
+
+  const footerText: HTMLElement = document.createElement('p');
+
+  const copyrightText: HTMLElement = document.createElement('span');
+  copyrightText.classList.add('copyright_text');
+  copyrightText.innerText = '© Oskar Landström 2024';
+
+  footerText.appendChild(copyrightText);
+
+  footerContainer.append(footerText, iconImg);
+
+  document.body.appendChild(footerContainer);
+}
