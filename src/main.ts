2e44d2088f76f1ca435f6e70ac6a07bd71a146de
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-03-28 11:45:05 +0100

    :art: feat(app): moves out mainContainer to main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,8 @@ import printStartPage from './script/printStartPage.ts';
 import printNav from './script/printNav.ts';
 
 printNav();
-printStartPage();
+
+const mainContainer: HTMLDivElement | null = document.querySelector('#app');
+if (mainContainer) {
+  printStartPage(mainContainer);
+}
