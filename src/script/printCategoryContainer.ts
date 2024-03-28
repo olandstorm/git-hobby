d011a74bb45f416c5d6bfcdee491086055a9b54f
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-03-28 11:45:43 +0100

    :sparkles: feat(categories): prints divs for each category

---
+++ src/script/printCategoryContainer.ts
@@ -0,0 +1,15 @@
+import Images from './lib/Images.ts';
+
+export default function printCategoryContainer(
+  images: Images[],
+  mainContainer: HTMLDivElement
+) {
+  const categoriesSet = new Set(images.map((image) => image.category));
+
+  categoriesSet.forEach((category) => {
+    const categoryContainer = document.createElement('div');
+    categoryContainer.classList.add('category_container');
+    categoryContainer.textContent = category;
+    mainContainer.append(categoryContainer);
+  });
+}
