26c542dfd2d4a793cb6fb50f0b2cd8ff50df68a1
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-03-28 09:52:22 +0100

    :pencil2: typeo(Images): corrected typeo on url (CHERRY PICK THIS)

--- src/script/lib/Images.ts
+++ src/script/lib/Images.ts
@@ -2,7 +2,7 @@ export default class Images {
   constructor(
     public id: number,
     public name: string,
-    public ulr: string,
+    public url: string,
     public alt: string,
     public category: string
   ) {}
