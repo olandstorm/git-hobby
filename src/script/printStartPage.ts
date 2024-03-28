a7125fb9d2d47746c260e3f4fbcef705b59960da
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-03-28 11:45:43 +0100

    :sparkles: feat(categories): prints divs for each category

--- src/script/printStartPage.ts
+++ src/script/printStartPage.ts
@@ -1,12 +1,17 @@
-export default function printStartPage(): void {
-  const mainContainer: HTMLDivElement | null = document.querySelector('#app');
-  if (mainContainer) {
-    mainContainer.innerHTML = '';
-  }
+import Images from './lib/Images.ts';
+import imagesData from '../images.json';
+import printCategoryContainer from './printCategoryContainer.ts';
+
+const images: Images[] = imagesData.images;
+
+export default function printStartPage(mainContainer: HTMLDivElement): void {
+  mainContainer.innerHTML = '';
 
   const logoImg: HTMLImageElement = document.createElement('img');
   logoImg.src = './img/travels_logo_color.webp';
   logoImg.classList.add('logo_img');
 
-  mainContainer?.appendChild(logoImg);
+  mainContainer.appendChild(logoImg);
+
+  printCategoryContainer(images, mainContainer);
 }
